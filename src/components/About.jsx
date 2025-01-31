3488d7916cb6c4f34c0b44a1d665e956c02a6fd1
Thangapandi-K <thangapandi.chella@gmail.com>
2025-01-31 20:21:54 +0530

    typewriter sm screen fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,11 @@ import AboutImg from '../assets/images/about.png';
 const About = () => {
   return (
     <div className='container about row mb-5'>
-            <div className='aboutTxtContainer lh-lg col-md-6 col-sm-12'>
-                <div className='aboutTxt h-100 p-5 fs-3'>
-                    <div>Hi,</div>
-                    <div> I'm <span className='fs-2 fw-bold name-text'><TypeWriter text={"Thangapandi K"} delay={500} infinite/></span> </div>
-                    <p className='fsdText'>An Aspiring Full Stack Web Developer</p>
+            <div className='aboutTxtContainer col-md-6 col-sm-12'>
+                <div className='aboutTxt h-100 p-5'>
+                    <div className='fs-5'>Hi,</div>
+                    <div className='lh-lg'> I'm <span className='fs-4 fw-bold name-text'><TypeWriter text={"Thangapandi K"} delay={500} infinite/></span> </div>
+                    <p className='fsdText fs-6'>An Aspiring Full Stack Web Developer</p>
                     <div className='d-flex gap-5 fs-2'>
                         <a className='link-secondary link-dark-hover icon-social' href="https://www.linkedin.com/in/thangapandi-k/" target='_blank'><AiOutlineLinkedin/></a>
                         <a className='link-secondary link-dark-hover icon-social' href="mailto:thangapandi.chella@gmail.com"><AiOutlineMail/></a>
